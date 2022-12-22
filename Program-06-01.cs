e3c99e00e563850c96c8fecc13c3413ab2371f07
maria-osmolovskaya <119137585+maria-osmolovskaya@users.noreply.github.com>
2022-12-23 01:13:48 +0300

    Add files via upload

---
+++ Program-06-01.cs
@@ -0,0 +1,41 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+*/
+
+
+Int64 m;
+Int64 CurrentNumber;
+Int64 Counter=0;
+{
+    while (true) // проверка введенного значения
+        {
+            Console.Write("Введите количество чисел: ");
+                if (Int64.TryParse(Console.ReadLine(), out m))
+                    {
+                        break;
+                    }
+                    Console.WriteLine("Это не число. Введите число.");
+        }
+
+                               
+    for (int i=1; i<=m; i++)
+        {
+            while (true) // Просто проверка введенного значения
+                    {
+                        Console.Write("Введите {0} число: ",i);
+                        if (Int64.TryParse(Console.ReadLine(), out CurrentNumber))
+                        {
+                            break;
+                        }
+                        Console.WriteLine("Это не число. Введите число.");
+                    }
+                    if (CurrentNumber > 0) { Counter++; }
+        }
+                
+        Console.WriteLine("Количество положительных чисел = {0}", Counter);
+
+}
+ 
---
+++ Program-06-02.cs
@@ -0,0 +1,60 @@
+﻿/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+
+Double k1;
+Double k2;
+Double b1;
+Double b2;
+        
+    while (true) // проверка введенного значения
+        {
+            Console.Write("Введите число k1: ");
+            if (Double.TryParse(Console.ReadLine(), out k1))
+                {
+                    break;
+                }
+            Console.WriteLine("Это не число. Введите число");
+                }
+
+                while (true) // проверка введенного значения
+                {
+                    Console.Write("Введите число b1: ");
+                    if (Double.TryParse(Console.ReadLine(), out b1))
+                    {
+                        break;
+                    }
+                    Console.WriteLine("Это не число. Введите число");
+                }
+
+                while (true) // проверка введенного значения
+                {
+                    Console.Write("Введите число k2: ");
+                    if (Double.TryParse(Console.ReadLine(), out k2))
+                    {
+                        break;
+                    }
+                    Console.WriteLine("Это не число. Введите число");
+                }
+
+                while (true) // проверка введенного значения
+                {
+                    Console.Write("Введите число b2: ");
+                    if (Double.TryParse(Console.ReadLine(), out b2))
+                    {
+                        break;
+                    }
+                    Console.WriteLine("Это не число. Введите число");
+                }
+
+
+                if (k1 == k2) { Console.WriteLine("k1=k2! Линии параллельны и не пересекаются."); }
+                else { Console.WriteLine("Точка пересечения линий с координатами [x,y]: [{0},{1}]", Math.Round(1/k2*(b1-k1*b2/k2)/(1-k1/k2)-b2/k2,1), Math.Round((b1-k1*b2/k2)/(1-k1/k2),1));}
+                
+
+              
+        
